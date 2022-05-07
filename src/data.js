0b3b09a7dc46f13558286be0c9af2d1ef8f9c78a
Sean Ye <seanye24@gmail.com>
2022-05-07 16:05:47 -0700

    use json to hold data

---
+++ src/data.js
@@ -0,0 +1,104 @@
+export default {
+  experience: [
+    {
+      title: 'Incoming Front End Engineer Intern',
+      organization: { name: 'Meta', href: 'https://www.metacareers.com/' },
+      date: 'Jun 2022 - Sep 2022',
+      description: ['Service Efficiency and Capacity Tools'],
+    },
+    {
+      title: 'SDE Intern',
+      organization: {
+        name: 'Amazon',
+        href: 'https://www.amazonuniversity.jobs/',
+      },
+      link: {
+        name: 'GitHub',
+        href: 'https://github.com/jmazanec15/opensearch-knn-perf-tool',
+      },
+      date: 'Jun 2021 - Sep 2021',
+      description: [
+        "Designed and lead the development of a <b>Python</b> testing framework for <b>OpenSearch's</b> (Amazon Elasticsearch) <b>k-NN</b> plugin",
+        "<b>Automated</b> the benchmarking and detection of performance regressions in the plugin's <b>CI/CD</b> workflow, preventing potential production bugs and enabling faster/robust development",
+        'Leveraged <b>OO</b> and strong abstractions to maintain simplicity and extensibility while efficiently processing millions of documents per test',
+        'Utilized <b>Docker/Compose</b> for test isolation and automated deployment; <b>EC2</b> instances for scalability',
+      ],
+    },
+    {
+      title: 'Lead Front End Developer',
+      organization: {
+        name: 'Hollr Technologies',
+        href: 'https://www.joinhollr.com/',
+      },
+      date: 'Aug 2020 - Feb 2022',
+      description: [
+        'Developed <b>Chrome extension</b> at a startup facilitating users in streamlining the sharing of products with others such as Amazon, Netflix, Youtube',
+        'Used <b>React TypeScript</b> on the frontend and <b>Firebase/Firestore</b> to manage user settings and endorsements',
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: 'Path Maze Visualizer',
+      siteUrl: 'https://www.pathmazer.com/',
+      githubUrl: 'https://github.com/seanye24/pathmazer',
+      date: 'Summer 2020',
+      description: [
+        'Built <b>React</b> application to visualize different pathﬁnding and maze generation algorithms',
+        'Implemented 10 different algorithms, including <b>Dijkstra</b>, A* Search, Kruskal, Prim, etc.',
+        'Utilized in a class of <b>100+</b> to help learn and build intuition on algorithms',
+      ],
+    },
+    {
+      name: 'Todo List',
+      siteUrl: 'https://minimaltodos.com',
+      githubUrl: 'https://github.com/seanye24/todo',
+      date: 'Spring 2020',
+      description: [
+        'Built <b>full stack</b> web app for storing and managing todos with user authentication and a <b>RESTful</b> Todo API',
+        'Used <b>React</b> on the frontend with an emphasis on <b>Material Design</b> (Material-UI)',
+        'Used <b>Node.js / Express</b> for API requests and <b>MongoDB</b> for persisting data',
+      ],
+    },
+    {
+      name: 'Generals.io Bot',
+      githubUrl: 'https://github.com/seanye24/generalsio-bot',
+      date: 'Summer 2020',
+      description: [
+        'Built <b>Node.js</b> bot to play Generals.io, a fast-paced multiplayer strategy game',
+        'Became the <b>number one</b> ranked bot in the 2020 season in 3 weeks',
+      ],
+    },
+  ],
+  education: {
+    courses: [
+      'Software Engineering',
+      'Advanced Data Structures',
+      'Algorithms',
+      'Programming Languages',
+      'Computer Vision',
+      'Machine Learning',
+      'Search and Reasoning',
+    ],
+  },
+  honors: [
+    'National Merit Finalist',
+    'Warren Honor Society',
+    'National AP Scholar',
+  ],
+  skills: {
+    languages: [
+      'TypeScript/JavaScript',
+      'Python',
+      'Go',
+      'C/C++',
+      'Java',
+      'HTML5/CSS3',
+      'Haskell',
+      'Bash',
+      'SQL',
+    ],
+    tools: ['AWS', 'Docker', 'Firebase', 'Git', 'Neovim', 'Linux'],
+    frameworks: ['React.js', 'Node.js', 'PostgreSQL', 'MongoDB', 'Qt'],
+  },
+};
---
+++ src/resume.js
@@ -0,0 +1,78 @@
+import data from './data.js';
+
+const experienceSection = document.querySelector('#experience');
+data.experience.forEach((exp) => {
+  const section = document
+    .querySelector('#experience-section')
+    .content.cloneNode(true);
+  section.querySelector('.experience-pos').textContent = exp.title;
+  section.querySelector('.experience-org a').textContent =
+    exp.organization.name;
+  section
+    .querySelector('.experience-org a')
+    .setAttribute('href', exp.organization.href);
+  section.querySelector('.experience-logistics').textContent = exp.date;
+  if ('link' in exp) {
+    const link = document.createElement('a');
+    link.setAttribute('href', exp.link.href);
+    link.textContent = exp.link.name;
+    section.querySelector(
+      '.experience-logistics',
+    ).textContent = ` | ${exp.date}`;
+    section.querySelector('.experience-logistics').prepend(link);
+  }
+  exp.description.forEach((desc) => {
+    const child = document.createElement('li');
+    child.innerHTML = desc;
+    section.querySelector('ul').appendChild(child);
+  });
+  experienceSection.appendChild(section);
+});
+
+const projectsSection = document.querySelector('#projects');
+data.projects.forEach((proj) => {
+  const section = document
+    .querySelector('#projects-section')
+    .content.cloneNode(true);
+  section.querySelector('.project-name').textContent = proj.name;
+
+  if (proj.siteUrl == null) {
+    section.querySelector('.project-logistics').childNodes[0].remove();
+    section.querySelector('.project-logistics').childNodes[0].remove();
+    section.querySelector('.project-logistics').childNodes[0].remove();
+  } else {
+    section
+      .querySelector('.project-site-url')
+      .setAttribute('href', proj.siteUrl);
+  }
+
+  section
+    .querySelector('.project-github-url')
+    .setAttribute('href', proj.githubUrl);
+  section
+    .querySelector('.project-logistics')
+    .appendChild(document.createTextNode(proj.date));
+  proj.description.forEach((desc) => {
+    const child = document.createElement('li');
+    child.innerHTML = desc;
+    section.querySelector('ul').appendChild(child);
+  });
+  projectsSection.appendChild(section);
+});
+
+document.querySelector('.relevant-coursework').textContent +=
+  ' ' + data.education.courses.join(', ');
+
+document
+  .querySelector('#honors')
+  .appendChild(document.createTextNode(data.honors.join(' • ')));
+
+const [languages, tools, frameworks] =
+  document.querySelector('.skills').children;
+languages.after(
+  document.createTextNode(' ' + data.skills.languages.join(' • ')),
+);
+tools.after(document.createTextNode(' ' + data.skills.tools.join(' • ')));
+frameworks.after(
+  document.createTextNode(' ' + data.skills.frameworks.join(' • ')),
+);
