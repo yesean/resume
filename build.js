b541cdc50d463ecbbb003720a2c084e7ea803495
Sean Ye <seanye24@gmail.com>
2022-09-13 17:19:11 -0700

    Replace esbuild-plugin-yaml with inline parser

--- build.js
+++ build.js
@@ -1,10 +1,33 @@
+const yaml = require('js-yaml');
+const path = require('path');
+const fs = require('fs');
+
+const yamlPlugin = () => ({
+  name: 'yaml',
+  setup(build) {
+    build.onLoad({ filter: /.yaml$/ }, async (args) => {
+      const content = yaml.load(fs.readFileSync(args.path, 'utf8'));
+      return {
+        contents: JSON.stringify(content),
+        loader: 'json',
+      };
+    });
+  },
+});
+
 require('esbuild').build({
   entryPoints: { index: 'src/resume.js' },
   bundle: true,
   metafile: true,
   outdir: 'dist/',
+  watch: process.env.MODE === 'dev' && {
+    onRebuild(error, result) {
+      if (error) console.error('watch build failed:', error);
+      else console.log('watch build succeeded:', result);
+    },
+  },
   plugins: [
-    require('esbuild-plugin-yaml').yamlPlugin(),
+    yamlPlugin(),
     require('esbuild-copy-static-files')({
       src: 'src/assets',
       dest: 'dist',
